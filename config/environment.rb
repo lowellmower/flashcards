ae537a62731cf20359fd8a4fe7ed5e2476d350c5
Lowell Mower <lowell.m.mower@gmail.com>
2015-06-12 17:31:46 -0400

    Update seed file for state deck and two users

--- config/environment.rb
+++ config/environment.rb
@@ -20,11 +20,6 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 require 'bcrypt'
-<<<<<<< HEAD
-=======
-
->>>>>>> lowell-models
-
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 

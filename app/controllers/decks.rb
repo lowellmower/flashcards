f47d5a7c4564c76d0168e985224974b5d920c797
Lowell Mower <lowell.m.mower@gmail.com>
2015-06-13 11:15:51 -0400

    add card in deck cycling

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -9,19 +9,23 @@ get '/decks/:id' do
 end
 
 post '/decks/:id' do
-# yes, deck ID
   @deck = Deck.find(params[:id])
   @round = Round.where(deck_id: params[:id], user_id: session[:user_id]).first_or_create
+  @current_card = @deck.cards.first
 
   if params[:current_card]
     @current_card = @deck.cards.find(params[:current_card])
     if Deck.compare_question_answer(params[:answer], @current_card.answer)
-      # create guess correct true
-      p Round.find(@round.id).guesses.build(correct: true, card_id: @current_card.id, round_id: @round.id).save
+      # create guess correct true ***create helper methods****
+      session[:message] = ["Correct!"]
+      Round.find(@round.id).guesses.build(correct: true, card_id: @current_card.id, round_id: @round.id).save
     else
-      # create guess correct false
+      session[:message] = ["Sorry, the answer was #{@current_card.answer}"]
+      # create guess correct false ***create helper methods****
+      Round.find(@round.id).guesses.build(correct: false, card_id: @current_card.id, round_id: @round.id).save
     end
     # increment card
+    @current_card = @deck.cards.find(@current_card.id += 1)
   end
   erb :'/decks/show'
 end

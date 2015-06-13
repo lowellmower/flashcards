68d939420d600e2ba39767ff4a04db2e76aa4acc
Lowell Mower <lowell.m.mower@gmail.com>
2015-06-13 08:38:21 -0400

    add class methods and tested logic for answer comparison

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -28,5 +28,6 @@ end
 
 get '/logout' do
   session.clear
+  redirect '/'
 end
 
--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -13,5 +13,12 @@ post '/decks/:id' do
   @deck = Deck.find(params[:id])
   @round = Round.where(deck_id: params[:id], user_id: session[:user_id]).first_or_create
 
+  if params[:current_card]
+    @current_card = @deck.cards.find(params[:current_card])
+    if Deck.compare_question_answer(params[:answer], @current_card.answer)
+      redirect '/'
+    end
+  end
+  # if compare_question_answer(params[:answer],
   erb :'/decks/show'
 end
